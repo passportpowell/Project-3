562fd94f89c6c6998b4d505e1d8e8c3a201b7af9
passportpowell <otisofro@gmail.com>
2022-10-07 15:51:50 +0000

    Code working, deleted unneeded code. To fix need to add hangman image progress

--- run.py
+++ run.py
@@ -1,70 +1,134 @@
-#Attempt to use DEF
-#attempt to use refactoring
-#attempt to decrement scores/timer
-#attempt to allow user to enter their name
-#attempt to add instruction on how to play
-#attempt to have correct letters show in terminal while game is playing
-#attempt to have player not be able to choose same letter multiple times per game
-#attempt to restrict player from using numbers and punctation.
+# attempt to use DEF
+# attempt to import somthing and use it
+# attempt to use refactoring
+# attempt to decrement scores/timer
+# attempt to allow user to enter their name
+# attempt to add instruction on how to play
+# attempt to have correct letters show in terminal while game is playing
+# attempt to have player not be able to choose same letter multiple times per game
+# attempt to restrict player from using numbers and punctation.
 
 # to fix - decrement for attempts left show up twice. one with just the number and 
 # the other time w attempts left
-#to fix - last letter doesn't show when all answers are correct
+# to fix - last letter doesn't show when all answers are correct
 # to fix - use DEF function in code
+# to fix - Game wont end after correctly guessing
+# to fix - attempts won decrement. Fixed by realising that i had not spell checked.
+# To fix - have image of a hangman progress each time answer is wrong
+
+
 
 import random
+import string
 
-#countries of europe to choose from
-# country =  ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czechia',
-# 'Denmark','Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Ireland',
-# 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands','Poland', 
-# 'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden']
+#  countries of europe to choose from
+country = ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czechia',
+'Denmark','Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Ireland',
+'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands','Poland',
+'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden']
+print(country)
 
-country = ['Slovakia', 'Slovenia', 'Spain', 'Sweden'] # to fix - use code above for country as this is for testing purpose
+def pick_country():
+    word = random.choice(country)    
+    while '-' in word or ' ' in word:
+        word = random.choice(country)
+    print(word)  #  print(word) # to fix - remove this as it will show the answer
+    return word.upper()
+ 
+ 
+def hang_game(word):
+    country_chosen = "_" * len(word)  # Puts the chosen country as "_" for each letter in the word 
 
-# print(country)
+    player_won = False
+    used_letters = []
+    used_words = []
+    attempts_left = 10
 
-word = random.choice(country).lower()
-# print(word) # to fix - remove this as it will show the answer
+    print("-----------------------------------------------------------------")
+    print("Welcome to Hangman!")
+    print(f"You have", {attempts_left}, "attempts to guess the country name")
+    print("----------------------------------------------------------------- \n")
+
+    print(word)
+    print("\n")
+
+    while not player_won and attempts_left > 0:
 
 
-right_guess = []
-wrong_guess = []
-attempts_left = 6
-hang_count = -1
 
+        # Forcing player input the be uppercase for equality check later ==     
+        player_guess = input("Please guess a letter or word: ").upper()
 
-while attempts_left > 0:
-    chosen_letter = ''
-    for letter in word:
-        if letter in right_guess:
-            chosen_letter += letter
+        # if loop for 3 conditions (Word, Letter or other which should result in an invalid message))
+
+        # if guess is a letter character and a letter a-z (.isalpha)
+        if len(player_guess) == 1 and player_guess.isalpha():
+            if player_guess in used_letters:
+                # Used ' '.join(player_guess)) so it converts my set into individual letters --> 'e t c'
+                print("You've already tried the letter: ", player_guess)
+                print("So far you have chosen the letters: ", " ".join(used_letters))
+                
+            elif player_guess not in word:
+                print(player_guess, "is not in the chosen Country. Try again!")
+                # Use += or -+
+                attempts_left -= 1
+                print("You Have", attempts_left)
+                #  print()   To fix - have image of a hangman each time answer is wrong                
+                used_letters.append(player_guess)  # adds letter chosen to list
+
+            else:
+                print("Excellent,", player_guess, "'is in the chosen Country. Keep Going!")
+                used_letters.append(player_guess)
+                # word_list = [letter if letter in used_letters or used_words else '_' for letter in word_completion]
+                # print('Country: ', ' '.join(word_list))
+ 
+                word_as_list = list(country_chosen)
+                indices = [i for i, letter in enumerate(word) if letter == player_guess]
+                for index in indices:
+                    word_as_list[index] = player_guess
+                country_chosen = "".join(word_as_list)
+                if "_" not in country_chosen:
+                    player_won = True
+
+        # if guess is a whole word and characters are from a-z (.isalpha)              
+        elif len(player_guess) == len(word) and player_guess.isalpha():
+
+            if player_guess in used_words:
+                print("You already Tried the Country", player_guess)
+
+            elif player_guess != word:
+                print(player_guess, "is not the Country.")
+                attempts_left -= 1
+                print("You Have", attempts_left)
+                #  print()   To fix - have image of a hangman each time answer is wrong
+                used_words.append(player_guess)
+
+            else:
+                player_won = True  # If player manages to work out the country the game should end here and offer a new game
+                country_chosen = word
+
         else:
-            chosen_letter += '_ '
-    if chosen_letter == word:
-        break
-    print(f"Guess the word, (Clue - {len(word)} Letters): ",chosen_letter)
-    print(attempts_left," chances left \n")
-    guess = input("Enter Guess Here: \n").lower()
-    # code below is checking if we've already used a letter before 
-    # regardlesws if it was correct or not
-    if guess in right_guess or guess in wrong_guess:
-        print("Already guessed", guess)
+            print("Not a valid guess.")
+        print(country_chosen)
+        print("\n")
 
-    #code below checks if guessed letter is in the word
-    elif guess in word:
-        print("Awesome Job! You guessed the correct letter!")
-        right_guess.append(guess)
+    if player_won:
+        print('YAY! You guessed the word', word.upper(), '!!')
 
-    #code below if for a incorrect guess and will decremnt the attempts by 1
+    # Game over or Congratulations code
     else:
-        print("Sorry! You have guessed a wrong letter!")
-        hangman_count = hang_count + 1
-        attempts_left = attempts_left - 1 # to fix - attempts left wont decrement instead just shows -1
-        wrong_guess.append(guess)
-        print(attempts_left) # to fix - Print an image of hangman for each wrong answer
+        print("Your Attempts Are Over. You Were So Close, Try again.")
+        print('The word was: ', word.upper()) 
+ 
+def main():
+    word = pick_country()
+    hang_game(word)
 
-if attempts_left > 0:
-    print(f"You Got The Country {word.upper()}, Congratulations!!!")
-else:
-    print("Your Attempts Are Over. You Were So Close Try again.")
+    # While loop to get game to restart from the beginning if player put Y or YES in
+    while input("hang_game Again? (Y-YES/N-NO) ").upper() == "Y" or "YES":
+        word = pick_country()
+        hang_game(word)
+ 
+ 
+if __name__ == "__main__":
+    main()
--- run2.py
+++ run2.py
@@ -1,94 +1,134 @@
-# #Attempt to use DEF
-# #attempt to use refactoring
-# #attempt to decrement scores/timer
-# #attempt to allow user to enter their name
-# #attempt to add instruction on how to play
-# #attempt to have correct letters show in terminal while game is playing
-# #attempt to have player not be able to choose same letter multiple times per game
-# #attempt to restrict player from using numbers and punctation.
+# attempt to use DEF
+# attempt to import somthing and use it
+# attempt to use refactoring
+# attempt to decrement scores/timer
+# attempt to allow user to enter their name
+# attempt to add instruction on how to play
+# attempt to have correct letters show in terminal while game is playing
+# attempt to have player not be able to choose same letter multiple times per game
+# attempt to restrict player from using numbers and punctation.
 
-# # to fix - decrement for attempts left show up twice. one with just the number and
-# # the other time w attempts left
+# to fix - decrement for attempts left show up twice. one with just the number and 
+# the other time w attempts left
+# to fix - last letter doesn't show when all answers are correct
+# to fix - use DEF function in code
+# to fix - Game wont end after correctly guessing
+# to fix - attempts won decrement. Fixed by realising that i had not spell checked.
+# To fix - have image of a hangman progress each time answer is wrong
 
 
-# #countries of europe to choose from
+
+import random
+import string
+
+#  countries of europe to choose from
 country = ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czechia',]
 # 'Denmark','Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Ireland',
 # 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands','Poland',
 # 'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden']
+# print(country)
 
-    # word = valid_country(country)
-    # letters_in_word = [(word)]
-    # guessed_letters = []
-    # alphab = [set(string.ascii_uppercase)]
-    # right_guess = []
-    # wrong_guess = []
-    # attempts_left = 6
-    # hang_count = -1
-    # player_guess = []
-
-import random
-import string
- 
- 
-def pick_country(country):
-    word = random.choice(country)  # randomly chooses something from the list
+def pick_country():
+    word = random.choice(country)    
     while '-' in word or ' ' in word:
         word = random.choice(country)
-    print(word)
+    print(word)  #  print(word) # to fix - remove this as it will show the answer
     return word.upper()
  
  
-def hang_game():
-    word = pick_country(country)
-    letters_in_word = [(word)]
-    guessed_letters = []
-    alphab = set(string.ascii_uppercase)
-    right_guess = []
-    wrong_guess = []
-    attempts_left = 6
-    hang_count = -1
-    player_guess = set() # this was a list which i tried to subtract from a set "alphab = set(string.ascii_uppercase)" - changed to set
- 
-    attempts_left = 50
- 
-    # getting user input
-    while len(letters_in_word) > 0 and attempts_left > 0:
-        # letters used
-        # Used ' '.join(player_guess)) so it converts my set into individual letters --> 'e t c'
-        print(attempts_left, ' Attempts left - So far you have chosen the letters: ', ' '.join(player_guess))
- 
-        # Puts the chosen country as "_" for each letter in the word 
-        word_list = [letter if letter in player_guess else '_' for letter in word]
-        print('Country: ', ' '.join(word_list))
+def hang_game(word):
+    country_chosen = "_" * len(word)  # Puts the chosen country as "_" for each letter in the word 
 
-        # Forcing player input the be uppercase for equality check later ==
-        player_letter = input('Guess a letter: ').upper()
- 
-        # to fix - keep failing when got to this point. problem is that i apparently cant subtract list from a list or set to list
-        if player_letter in alphab - player_guess:
- 
-            # to fix cant append as  i've changed from a list to a set. must add
-            player_guess.add(player_letter)
-            if player_letter in letters_in_word:
-                letters_in_word.remove(player_letter)
-                print('')
- 
+    player_won = False
+    used_letters = []
+    used_words = []
+    attempts_left = 10
+
+    print("-----------------------------------------------------------------")
+    print("Welcome to Hangman!")
+    print(f"You have", {attempts_left}, "attempts to guess the country name")
+    print("----------------------------------------------------------------- \n")
+
+    print(word)
+    print("\n")
+
+    while not player_won and attempts_left > 0:
+
+
+
+        # Forcing player input the be uppercase for equality check later ==     
+        player_guess = input("Please guess a letter or word: ").upper()
+
+        # if loop for 3 conditions (Word, Letter or other which should result in an invalid message))
+
+        # if guess is a letter character and a letter a-z (.isalpha)
+        if len(player_guess) == 1 and player_guess.isalpha():
+            if player_guess in used_letters:
+                # Used ' '.join(player_guess)) so it converts my set into individual letters --> 'e t c'
+                print("You've already tried the letter: ", player_guess)
+                print("So far you have chosen the letters: ", " ".join(used_letters))
+                
+            elif player_guess not in word:
+                print(player_guess, "is not in the chosen Country. Try again!")
+                # Use += or -+
+                attempts_left -= 1
+                print("You Have", attempts_left)
+                #  print()   To fix - have image of a hangman each time answer is wrong                
+                used_letters.append(player_guess)  # adds letter chosen to list
+
             else:
-                attempts_left = attempts_left - 1  # takes away a life if incorrect
-                print('\n',player_letter, 'is not in the chosen Country. Try again!')
-        # Wont reduce attempts left if player uses a letter again
-        elif player_letter in player_guess:
-            print('\nYou have already tried that letter. Try again!')
-        # due to import string and alphab = set(string.ascii_uppercase) player can only use letters a - z
+                print("Excellent,", player_guess, "'is in the chosen Country. Keep Going!")
+                used_letters.append(player_guess)
+                # word_list = [letter if letter in used_letters or used_words else '_' for letter in word_completion]
+                # print('Country: ', ' '.join(word_list))
+ 
+                word_as_list = list(country_chosen)
+                indices = [i for i, letter in enumerate(word) if letter == player_guess]
+                for index in indices:
+                    word_as_list[index] = player_guess
+                country_chosen = "".join(word_as_list)
+                if "_" not in country_chosen:
+                    player_won = True
+
+        # if guess is a whole word and characters are from a-z (.isalpha)              
+        elif len(player_guess) == len(word) and player_guess.isalpha():
+
+            if player_guess in used_words:
+                print("You already Tried the Country", player_guess)
+
+            elif player_guess != word:
+                print(player_guess, "is not the Country.")
+                attempts_left -= 1
+                print("You Have", attempts_left)
+                #  print()   To fix - have image of a hangman each time answer is wrong
+                used_words.append(player_guess)
+
+            else:
+                player_won = True  # If player manages to work out the country the game should end here and offer a new game
+                country_chosen = word
+
         else:
-            print('\nThat is not a valid letter. Try again!')
- 
-    # Game over or Congratulations code
-    if attempts_left == 0:
-        print("Your Attempts Are Over. You Were So Close, Try again.")
-        print('The word was', word)
-    else:
+            print("Not a valid guess.")
+        print(country_chosen)
+        print("\n")
+
+    if player_won:
         print('YAY! You guessed the word', word.upper(), '!!')
+
+    # Game over or Congratulations code
+    else:
+        print("Your Attempts Are Over. You Were So Close, Try again.")
+        print('The word was: ', word.upper()) 
  
-hang_game()
+def main():
+    word = pick_country()
+    hang_game(word)
+
+    # While loop to get game to restart from the beginning if player put Y or YES in
+    while input("hang_game Again? (Y-YES/N-NO) ").upper() == "Y" or "YES":
+        word = pick_country()
+        hang_game(word)
+ 
+ 
+if __name__ == "__main__":
+    main()
